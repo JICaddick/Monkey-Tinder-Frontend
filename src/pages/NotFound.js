6c78bfc6f1a6a42cc62f0dba06fdeea7f6180943
JICaddick <james_caddick16@hotmail.com>
2022-08-02 17:29:16 -0500

    frontend structure done

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export class NotFound extends Component {
   render() {
     return (
+      <>
       <div>NotFound</div>
+      <div className='not-found'></div>
+      </>
     )
   }
 }
