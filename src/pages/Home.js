05d5cd050fc6da4616d0383afbc15ce176c8b672
JICaddick <james_caddick16@hotmail.com>
2022-08-07 15:06:04 -0500

    Home styled- navbar styling to go

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import homemonkey from "../assets/home-monkey.jpeg"
 
 class Home extends Component {
   render() {
     return (
       <>
-        <h3>Welcome to Monkey Tinder</h3>
+        <div className="home">
+          <center>
+            <h3>Home</h3>
+            <img style= {{height: "300px", width:"auto" }} src={homemonkey} ></img>
+          </center>
+        </div>
       </>
     )
   }
